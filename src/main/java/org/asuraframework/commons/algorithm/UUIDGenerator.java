a4298092f61707cb532338fe8ba694669b4c0d42
liusq23 <liusq23@ziroom.com>
2018-05-01 00:24:29 +0800

    add java type

---
+++ src/main/java/org/asuraframework/commons/algorithm/UUIDGenerator.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright(c) 2018 asura
+ */
+package org.asuraframework.commons.algorithm;
+
+/**
+ * <p></p>
+ *
+ *
+ * @author liusq23
+ * @since 1.0
+ * @version 1.0
+ * @Date 2018/4/2 下午9:31
+ */
+public class UUIDGenerator {
+}
